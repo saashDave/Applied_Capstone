94cdf9e442812cb946f393f7efceba6909c97ec9
sharon <34096667+saashDave@users.noreply.github.com>
2018-11-27 11:24:34 -0500

    Add files via upload

---
+++ Search/README.md
@@ -0,0 +1,6 @@
+# Search
+
+<a href="https://imgflip.com/gif/1z3lsu"><img src="https://i.imgflip.com/1z3lsu.gif" title="made at imgflip.com"/></a>
+
+
+# For more info, please visit <a href="https://www.sharon-devasia.com" 
---
+++ Search/places/NearbyCell.swift
@@ -0,0 +1,24 @@
+//
+//  nearbyCell.swift
+//  places
+//
+//  Created by Sharon Devasia on 11/8/18.
+//  Copyright © 2018 Sharon Devasia. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class NearbyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+    func updateUI(responseResult: MKMapItem) {
+       // cellTitle
+    }
+
+}
+
